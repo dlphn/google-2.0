5ac54fc9ee9c297040456b1ed4d35164fa1d977a
Delphine Shi <shi.delphine@gmail.com>
2018-12-10 11:09:04 +0100

    Set tokens to lowercase and remove stop words

--- textProcessing.py
+++ textProcessing.py
@@ -5,16 +5,29 @@ from nltk.tokenize import RegexpTokenizer
 class TextProcessor:
 
     def __init__(self):
-        nltk.download('punkt')
+        # nltk.download('punkt')
         self.tokenizer = RegexpTokenizer(r'\w+')
         self.tokens = []
+        self.text = ""
 
-    def tokenize(self, text):
-        self.tokens = self.tokenizer.tokenize(text)
-        print(self.tokens)
+    def process(self, text):
+        self.text = text
+        self.tokenize()
+        self.remove_stop_words()
+        print(self.tokens, len(self.tokens))
+
+    def tokenize(self):
+        self.tokens = self.tokenizer.tokenize(self.text)
+        # set all tokens to lowercase
+        self.tokens = [token.lower() for token in self.tokens]
+
+    def remove_stop_words(self):
+        with open("CACM/common_words") as f:
+            stop_words = f.read()
+        self.tokens = [token for token in self.tokens if token not in stop_words]
 
 
 if __name__ == "__main__":
     processor = TextProcessor()
     sentence = 'Eighty-seven miles to go, yet.  Onward!'
-    processor.tokenize(sentence)
+    processor.process(sentence)
