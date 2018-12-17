d1f4af4e31b9122e6e917233b52c26a97bbf2be1
Cecile Gontier <cecile.gontier@student.ecp.fr>
2018-12-17 11:02:31 +0100

    added a parse_all function that recovers the title, summary and keywords of each document

--- CACMParser.py
+++ CACMParser.py
@@ -26,41 +26,61 @@ class CACMParser:
         return dic.keys()
 
     def parse_title(self, dic):
+        dic2 = {}
         for id in dic.keys():
             doc = dic[id]
             title = None
             if len(doc.split(title_separator)) == 2:
                 title = doc.split(title_separator)[1].split("\n")[0]
-            dic[id] = title
-        return dic
+            dic2[id] = title
+        return dic2
 
     def parse_summary(self, dic):
+        dic2 = {}
         for id in dic.keys():
             doc = dic[id]
             summary = None
             if len(doc.split(summary_separator)) == 2:
                 summary = doc.split(summary_separator)[1].split(".B")[0]
-            dic[id] = summary
-        return dic
+            dic2[id] = summary
+        return dic2
 
     def parse_key_words(self, dic):
+        dic2 = {}
         for id in dic.keys():
             doc = dic[id]
             key_words = None
             if len(doc.split(key_words_separator)) == 2:
                 key_words = doc.split(key_words_separator)[1].split(".C")[0]
-            dic[id] = key_words
-        return dic
+            dic2[id] = key_words
+        return dic2
 
+    def parse_all(self, dic):
+        titles = self.parse_title(dic)
+        summaries = self.parse_summary(dic)
+        keywords = self.parse_key_words(dic)
+        all = {}
+        for id in dic.keys():
+            all[id] = titles[id]
+            if summaries[id] is not None:
+                all[id] = all[id] + " " + summaries[id]
+            if keywords[id] is not None:
+                all[id] = all[id] + " " + keywords[id]
+        return all
 
 parser = CACMParser()
 with open("CACM/cacm.all") as f:
     read_data = f.read()
 
 dic = parser.parse_documents(read_data)
-print(dic.keys())
+#print(dic.keys())
+#print(dic)
+
+# print(parser.parse_key_words(dic))
+
+print(parser.parse_all(dic))
 
 # print(parser.parse_title(dic))
 # print(parser.parse_summary(dic))
-# print(parser.parse_key_words(dic))
+
 
