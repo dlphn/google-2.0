e1abc1630fefe03ef574460452248e8109e7fd5a
Cecile Gontier <cecile.gontier@student.ecp.fr>
2018-12-30 18:32:20 +0100

    Added class to calculate Heap law parameters by linear regression

--- CACMIndex.py
+++ CACMIndex.py
@@ -8,9 +8,11 @@ class CACMIndex:
         self.parser = CACMParser.CACMParser()
         self.index = None
 
-    def build(self):
+    def build(self, half=False):
         with open("CACM/cacm.all") as f:
             read_data = f.read()
+        if half:
+            read_data = read_data[:len(read_data)//2]
         dic = self.parser.parse_documents(read_data)
         data = self.parser.parse_all(dic)
         self.index = indexBuilder.IndexBuilder('CACM', data)
@@ -27,5 +29,8 @@ class CACMIndex:
 if __name__ == "__main__":
     index = CACMIndex()
     index.build()
-    index.get_tokens()
-    index.get_vocabulary()
+    # index.get_tokens()
+    # index.get_vocabulary()
+
+    print("For half of the text:")
+    index.build(half=True)
--- CS276Index.py
+++ CS276Index.py
@@ -7,13 +7,15 @@ class CS276Index:
     def __init__(self):
         self.index = None
 
-    def build(self):
+    def build(self, half=False):
         data = ""
         for file_id in range(10):
             for filename in os.listdir("pa1-data/" + str(file_id)):
                 with open("pa1-data/" + str(file_id) + "/" + filename) as f:
                     read_data = f.read()
                     data = data + read_data
+        if half:
+            data = data[:len(data)//2]
         self.index = indexBuilder.IndexBuilder('CS276', data)
         self.index.build()
         self.index.get_size()
@@ -27,9 +29,12 @@ class CS276Index:
 
 if __name__ == "__main__":
     index = CS276Index()
-    index.build()
+    index.build(half=True)
     # index.get_tokens()
     # index.get_vocabulary()
+
+    print("For half of the text:")
+    index.build(half=True)
 
 
 
