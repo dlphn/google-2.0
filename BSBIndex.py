357da54830aa53974f224b2c4ad0b08db3d18ca6
Delphine Shi <shi.delphine@gmail.com>
2019-01-02 15:22:23 +0100

    BSBIndex setup

---
+++ BSBIndex.py
@@ -0,0 +1,16 @@
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s : %(message)s', level=logging.INFO)
+
+
+class BSBIndex:
+    """
+    Build a Block Sort-based index
+    """
+
+    def __init__(self):
+        print('hey')
+
+
+if __name__ == "__main__":
+    index = BSBIndex()
