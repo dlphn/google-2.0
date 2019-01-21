390a884abbf7b7b16afcfdf61a8fd15661c8316b
Cecile Gontier <cecile.gontier@student.ecp.fr>
2019-01-21 18:03:07 +0100

    parse qrels.text and compare it agaigainst our results

--- vectorial/test.py
+++ vectorial/test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from helpers.CACMParser import CACMParser
+from vectorial.qrelsParser import QrelsParser
 from vectorial.vectorialEvaluation import VectorialEvaluation
 from vectorial.naturalWeighting import *
 from vectorial.tfidfWeighting import *
@@ -14,20 +15,35 @@ class TestEvaluation(unittest.TestCase):
         results = model.search(NaturalWeighting())
         self.assertEqual(results.tolist(), [1258, 2967, 1718, 2744, 1409])
 
-    def test_CACM_vectorial(self):
-        parser = CACMParser()
+    def test_against_qrels(self):
+        cacm_parser = CACMParser()
         with open("../CACM/query.text") as f:
-            read_data = f.read()
+            cacm_data = f.read()
 
-        dic = parser.parse_documents(read_data)
-        requests = parser.parse_summary(dic)
+        dic = cacm_parser.parse_documents(cacm_data)
+        requests = cacm_parser.parse_summary(dic)
+
+        qrels_parser = QrelsParser()
+        with open("../CACM/qrels.text") as f:
+            qrels_data = f.read()
+
+        expected = qrels_parser.parse_all(qrels_data)
+
+        matching_results = {key: [] for key in range(1, 65)}
 
         for request_id, request in requests.items():
             model = VectorialEvaluation(request, "CACM")
             # results = model.search(NaturalWeighting())
             results = model.search(TfIdfWeighting())
-            print(request_id, results)
-            self.assertEqual(0, 0)
+            try:
+                for doc_id in expected[int(request_id)]:
+                    if doc_id in results[0]:
+                        matching_results[int(request_id)].append(doc_id)
+            except KeyError:
+                pass
+            # self.assertIn(expected[int(request_id)], results)
+        for key, value in matching_results.items():
+            print("{0}: {1}".format(key, value))
 
 
 if __name__ == "__main__":
