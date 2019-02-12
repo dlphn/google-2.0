e6380cf4e2c6e7e45f42c5337be83953c3452dba
Cecile Gontier <cecile.gontier@student.ecp.fr>
2019-02-12 18:44:50 +0100

    add e, f, r measures and mean average precision

--- CACMEvaluation.py
+++ CACMEvaluation.py
@@ -1,6 +1,7 @@
 from BSBIndex import BSBIndex
 from CACMIndex import CACMIndex
 import config
+import numpy as np
 
 import time
 import os
@@ -74,45 +75,53 @@ def test_CACM_against_qrels():
     expected = qrels_parser.parse_all(qrels_data)
     actual = {key: [] for key in range(1, 65)}
 
-    matching_results = {key: [] for key in range(1, 65)}
-
     for request_id, request in requests.items():
         model = VectorialEvaluation(request, "CACM")
-        # results = model.search(NaturalWeighting())
-        results = model.search_all(TfIdfWeighting())
+        # results = model.search_all(NaturalWeighting())
+        # results = model.search_all(TfIdfWeighting())
+        results = model.search_all(NormalizedTfIdfWeighting())
         actual[int(request_id)] = results[0]
-        try:
-            for doc_id in expected[int(request_id)]:
-                if doc_id in results[0]:
-                    matching_results[int(request_id)].append(doc_id)
-        except KeyError:
-            pass
-        # self.assertIn(expected[int(request_id)], results)
-    for key, value in matching_results.items():
-        print("{0}: {1}".format(key, value))
-        print("actual: {}".format(actual[key]))
-    return expected, actual, matching_results
+    return expected, actual
 
 
-def courbe_rappel_precision():
-    expected, actual, matching_results = test_CACM_against_qrels()
-    x = [0.1*n for n in range(10)]
-    y = [0.0 for _ in range(10)]
+def calculate_measures():
+    expected, actual = test_CACM_against_qrels()
+    courbe_rappel_precision(expected, actual)
+    calculate_r_measure(expected, actual)
+
+
+def courbe_rappel_precision(expected, actual):
+    print("Plotting recall-precision curve...")
+    x = [0.1*n for n in range(11)]
+    y = [0.0 for _ in range(11)]
+    e = [0.0 for _ in range(11)]
+    f = [0.0 for _ in range(11)]
+    avg_precision = []
     for request_id in expected.keys():
         rappel, precision = courbe(expected[request_id], actual[request_id])
-        for j in range(10):
+        avg_precision.append(np.average(precision))
+        for j in range(11):
             rj = x[j]
             for i,r in enumerate(rappel):
                 if r > rj:
                     y[j] += max(precision[i:])
                     break
-    for j in range(10):
+    for j in range(11):
         y[j] = y[j]/64
-    plt.plot(x, y, marker="x")
+
+    for j in range(11):
+        e[j] += e_measure(x[j], y[j])
+        f[j] += 1 - e[j]
+
+    plt.plot(x, y, marker="x", label='precision')
+    plt.plot(x, e, marker='x', label="e-measure")
+    plt.plot(x, f, marker='x', label="f-measure")
     plt.axis('equal')
     plt.xlabel('Recall')
-    plt.ylabel('Precision')
+    plt.legend()
     plt.show()
+    print("Recall-precision curve plotted")
+    print("The Mean Average Precision is {}".format(np.nanmean(avg_precision)))
 
 
 def courbe(expected, actual):
@@ -127,4 +136,28 @@ def courbe(expected, actual):
     return r, p
 
 
-courbe_rappel_precision()
+def calculate_r_measure(expected, actual):
+    for request_id in expected.keys():
+        r_prec = r_precision(expected[request_id], actual[request_id])
+        print("For the request {0} the r-precision is {1}.". format(request_id, r_prec))
+
+
+def r_precision(expected, actual):
+    r_prec = 0
+    for rank in range(len(expected)):
+        if len(actual) > rank and actual[rank] in expected:
+            r_prec += 1
+    return r_prec/len(expected)
+
+
+def e_measure(rappel, precision):
+    if rappel > 0:
+        beta = float(precision)/float(rappel)
+        e = 1 - (((beta * beta + 1) * rappel * precision) / (beta * beta * precision + rappel))
+        return e
+    else:
+        return 1
+
+
+
+calculate_measures()
